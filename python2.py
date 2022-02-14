5dffae570016ddb6f0c638490970933d49c71411
Daniel C <danielc6275@gmail.com>
2022-02-14 16:27:28 -0500

    Tuples

--- python2.py
+++ python2.py
@@ -1,1 +1,18 @@
-#tuples
+#tuples: ordered, unchangeable, allows duplication of elements
+thisTuple = ("Dan", 18, "Orlando")
+print(thisTuple)
+
+myTuple = ("Jimmy",)
+print(myTuple)
+
+someTuple = tuple(["Dan",18,"Orlandy"])
+someItem = thisTuple[-2]
+
+if "Dan" in thisTuple:
+    print("yea")
+else:
+    print("nah")
+
+a_tuple = ('b','a','n','a','n','a')
+print(someTuple.index('n'))
+print(len(someTuple))
