0d92f3d14b83b603e219a995ef77a91ed8a3c56c
Daniel C <danielc6275@gmail.com>
2022-02-15 21:48:06 -0500

    Dictionaries and sets

--- python2.py
+++ python2.py
@@ -7,12 +7,32 @@ print(myTuple)
 
 someTuple = tuple(["Dan",18,"Orlandy"])
 someItem = thisTuple[-2]
-
-if "Dan" in thisTuple:
-    print("yea")
-else:
-    print("nah")
-
 a_tuple = ('b','a','n','a','n','a')
 print(someTuple.index('n'))
 print(len(someTuple))
+
+a_list = list(a_tuple)
+print(a_list)
+
+a = (1,2,3,4,5,6,7,8,9,10)
+
+b = a[2:5]
+c = a[::-1]
+d = a[::]
+
+some_tuple = (0,1,2,3,4)
+i1, *i2, i3 = some_tuple
+
+print(i1)
+print(i3)
+print(i2)
+
+import sys
+
+d_list = [0,1,2,"hi",True]
+d_tuple = (0,1,2,"hi",True)
+print(sys.getsizeof(d_list),"bytesizes")
+print(sys.getsizeof(d_tuple),"bytesizes")
+
+import timeit
+print(timeit.timeit(stmt="[0,1,2,3,4,5]",number=100000))
---
+++ python3.py
@@ -0,0 +1,41 @@
+#dictionaries
+from this import d
+
+
+dictor = {"name": "Dan","age":"18","city":"Orlando"}
+print(dictor)
+
+dictor2 = dict(name="Dan", age="18",city="Orlando")
+
+dictor["email"] = "dc6275@gmail.com"
+print(dictor)
+
+del dictor["name"]
+print(dictor)
+
+dictor.pop("age")
+print(dictor)
+
+dictor.popitem()
+print(dictor)
+
+if "name" in dictor:
+    print(dictor["name"])
+
+try: 
+    print(dictor["name"])
+except:
+    print("error")
+
+for key in dictor:
+    print(key)
+
+for key,value in dictor.items():
+    print(value)
+
+dictor_cpy = dictor
+print(dictor_cpy)
+
+dictor_cpy["email"] = "dc6275@gmail.com"
+print(dictor_cpy)
+print(dictor)
